f2c485cf962bd36ada1b0a95f569e8ad8fb7b405
Matthies Abera <s3779748@student.rmit.edu.au>
2023-09-24 14:10:43 +1000

    changed burger image to constant size once desktop viewport is reached and set max width for location section p tag.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+/* TODO: FIX IMAGE ALIGNMENT IN LOCATION SECTION FOR DESKTOP VIEWPORT */
+/* TODO: ADJUST GRID LAYOUT IN LOCATION SECTION FOR DESKTOP VIEWPORT */
+
 import Navbar from '@/components/Navbar'
 import styles from './page.module.css'
 import Footer from '@/components/Footer'
@@ -9,19 +12,19 @@ export default function Home() {
     <div className={styles.home}>
       <Navbar />
 
-      <div className={styles.heroSection}>
+      <section className={styles.heroSection}>
         <video autoPlay muted loop className={styles.video}>         
           <source src='/Untitled.mov' type="video/mp4"/>       
         </video>
         <h1 className={styles.heroTitle}>SHISO BURGER:<br/>THE BEST OF TWO WORLDS</h1>
-      </div>
+      </section>
 
-      <div className={styles.locationSection}>
+      <section className={styles.locationSection}>
         <p><b>Do you like burgers and have a taste for Asian cuisine?</b> With us, you get both in one. Shiso Burger stands for freshly made burgers with fine ingredients – in our Berlin-Mitte home base and in many cities worldwide.</p>
         <Image src={require("../assets/images/fresh-burger-and-worker.avif")} className={styles.burgerImage} alt='resh-burger-and-worker'/>
         <p>Enjoy meat from Angus beef, refined spices and delicious sauces from our own production in homemade buns. On request, we serve our Asia Burger with some of our additional Extras, of your choosing.</p>
         <Link href='/location'><button>FIND A LOCATION NEAR YOU</button></Link>
-      </div>
+      </section>
 
       <Footer />
     </div>
