047faa9fb75d94dc7e36730bd5e9b8085e9e39a5
Matthies Abera <s3779748@student.rmit.edu.au>
2023-10-13 09:43:39 +1100

    base level styling for desktop viewport of MenuItem component - making adjustments next to make it more closely resemble shiso.

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -15,40 +15,46 @@ export default function MenuItem(data: MenuItem) {
     return (
         <section className={styles.menuItem}>
 
-            <h2>{data.name.toUpperCase()}</h2>
-            <div className={styles.subtype}>
-                {data.subtype.map(subtype =>
-                    <i className={styles.subtypeText} key={subtype}>{subtype}</i>
-                )} 
-            </div>
-            { data.description != null &&
-                <p className={styles.description}>{data.description}</p>
-            }
-            <Image src={data.image} alt={data.name} className={styles.itemImage}/>
-
-            <div className={styles.drownDownButton} onClick={() => handleClick()}>
-                <h3>WHAT&apos;S IN IT</h3>
-                <Image src={require('../assets/icons/ingredients.svg')} alt='ingredients image'/>
-            </div>
-
-            <div className={[styles.downDownInformation, isClosed && styles.downDownClosed].join(' ')}>
-                {data.ingredients.map(ingredient =>
-                    <p key={ingredient}>{ingredient}</p>
-                )}
-                
-                {data.allergens != null &&
-                    <>
-                        <h3 className={styles.allergenTitle}>ALLERGY INFO</h3>
-                        <div className={styles.allergens}>
-                            {data.allergens.map(allergen =>
-                                allergen != data.allergens![data.allergens!.length - 1]
-                                ? <p key={allergen}>{allergen}, </p>
-                                : <p key={allergen}>{allergen}.</p>
-                            )}
-                        </div>
-                    </>
+            <div className={styles.itemSummary}>
+                <h2>{data.name.toUpperCase()}</h2>
+                <div className={styles.subtype}>
+                    {data.subtype.map(subtype =>
+                        <i className={styles.subtypeText} key={subtype}>{subtype}</i>
+                    )} 
+                </div>
+                { data.description != null &&
+                    <p className={styles.description}>{data.description}</p>
                 }
             </div>
+            
+            <div className={styles.itemInformation}>
+                <Image src={data.image} alt={data.name} className={styles.itemImage}/>
+
+                <div className={styles.drownDownButton} onClick={() => handleClick()}>
+                    <h3>WHAT&apos;S IN IT</h3>
+                    <Image src={require('../assets/icons/ingredients.svg')} alt='ingredients image'/>
+                </div>
+
+                <div className={[styles.downDownInformation, isClosed && styles.downDownClosed].join(' ')}>
+                    {data.ingredients.map(ingredient =>
+                        <p key={ingredient}>{ingredient}</p>
+                    )}
+                    
+                    {data.allergens != null &&
+                        <>
+                            <h3 className={styles.allergenTitle}>ALLERGY INFO</h3>
+                            <div className={styles.allergens}>
+                                {data.allergens.map(allergen =>
+                                    allergen != data.allergens![data.allergens!.length - 1]
+                                    ? <p key={allergen}>{allergen}, </p>
+                                    : <p key={allergen}>{allergen}.</p>
+                                )}
+                            </div>
+                        </>
+                    }
+                </div>
+            </div>
+            
         </section>
     )
 }
